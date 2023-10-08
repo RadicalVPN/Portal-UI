a6765c101e60edd23c1f347c9986e7705dc0129f
Primexz <johann@pepebot.info>
2023-10-08 23:35:34 +0200

    feat(dashboard): response info cards

--- src/pages/admin/dashboard/DashboardInfoBlock.vue
+++ src/pages/admin/dashboard/DashboardInfoBlock.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid grid-cols-12 gap-6 pb-6">
-    <va-card class="col-span-12 sm:col-span-2" color="success">
+    <va-card class="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-2" color="success">
       <va-card-content>
         <va-inner-loading v-if="vpnsLoading" class="py-4 justify-center" :loading="true" />
         <h2 v-if="!vpnsLoading" class="va-h2 m-0 text-white">{{ store.vpns.length }}</h2>
@@ -8,7 +8,7 @@
       </va-card-content>
     </va-card>
 
-    <va-card class="col-span-12 sm:col-span-2" color="success">
+    <va-card class="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-2" color="success">
       <va-card-content>
         <va-inner-loading v-if="trafficLoading" class="py-4 justify-center" :loading="true" />
         <h2 v-if="!trafficLoading" class="va-h2 m-0 text-white">{{ store.humanFileSize(trafficDown) }}</h2>
@@ -16,7 +16,7 @@
       </va-card-content>
     </va-card>
 
-    <va-card class="col-span-12 sm:col-span-2" color="success">
+    <va-card class="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-2" color="success">
       <va-card-content>
         <va-inner-loading v-if="trafficLoading" class="py-4 justify-center" :loading="true" />
         <h2 v-if="!trafficLoading" class="va-h2 m-0 text-white">{{ store.humanFileSize(trafficUp) }}</h2>
