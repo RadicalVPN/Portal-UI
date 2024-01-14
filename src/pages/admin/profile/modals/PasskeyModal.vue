a0368745171a706ee7baec5392473f7764a35642
Primexz <johann@pepebot.info>
2024-01-14 23:32:13 +0100

    feat: basic webauthn setup with dummy data

--- src/pages/admin/profile/modals/PasskeyModal.vue
+++ src/pages/admin/profile/modals/PasskeyModal.vue
@@ -12,6 +12,23 @@
       password, or a PIN.
     </p>
 
+    <VaAlert v-if="!passkeysSupported" color="warning">
+      Your current browser does not seem to support Passkeys,
+    </VaAlert>
+
+    <VaAlert v-if="!!registrationError" color="danger" class="mb-6"> {{ registrationError }}. </VaAlert>
+
+    <div class="flex justify-end">
+      <VaButton
+        size="medium"
+        :disabled="!passkeysSupported"
+        :loading="isRegistrationRunning"
+        class="mb-3 mt-6"
+        @click="register"
+        >Add New Passkey</VaButton
+      >
+    </div>
+
     <VaDataTable class="table-crud mt-6" :items="passkeys" :columns="columns">
       <template #cell(actions)="{}">
         <VaButton preset="plain" icon="delete" class="ml-3" />
@@ -22,9 +39,13 @@
 <script lang="ts" setup>
   import { ref } from 'vue'
   import { useGlobalStore } from '../../../../stores/global-store'
+  import { client } from '@passwordless-id/webauthn'
+  import { RegistrationEncoded } from '@passwordless-id/webauthn/dist/esm/types'
 
-  //const store = useGlobalStore()
+  const store = useGlobalStore()
   const emits = defineEmits(['cancel'])
+  const registrationError = ref('')
+  const isRegistrationRunning = ref(false)
 
   const passkeys = ref([
     {
@@ -38,6 +59,43 @@
     { key: 'lastUsed', sortable: true },
     { key: 'actions', width: 60 },
   ]
+
+  const passkeysSupported = client.isAvailable()
+
+  async function register() {
+    registrationError.value = ''
+    isRegistrationRunning.value = true
+
+    //TOOD: GENERATE THIS ON THE SERVEr
+    const randomNum = Math.floor(Math.random() * 10)
+    const challenge = `a7c61ef9-dc23-4806-b486-2428938a54${randomNum}e`
+
+    console.log('Webauthn: Starting registration')
+    let registration: RegistrationEncoded
+    try {
+      registration = await client.register(store.user.email, challenge, {
+        authenticatorType: 'auto',
+        userVerification: 'required',
+        discoverable: 'required',
+        timeout: 60000,
+        attestation: false,
+      })
+    } catch (error: any) {
+      const msg = error.message
+
+      console.log('Webauthn: Registration failed', msg)
+      registrationError.value = msg.split('.')[0] ?? 'Unknown Webauthn error'
+      return
+    } finally {
+      isRegistrationRunning.value = false
+    }
+
+    console.log('WebAuthn: Registration complete')
+
+    console.log(registration)
+
+    //TODO: SEND REGISTRATION TO SERVER
+  }
 </script>
 
 <style lang="scss">
